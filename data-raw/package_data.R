de79d2dd39d323edc9575e596b628e3c976ee4d7
gileadekelvin <gileadekelvin@gmail.com>
2020-12-16 16:29:40 -0300

    Adiciona funções para processar o financiamento por setor Adiciona dados de cnaes

---
+++ data-raw/package_data.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+
+cnae_classificacao <- read_csv(here::here("data-raw/cnae_classificacao.csv"))
+usethis::use_data(cnae_classificacao, overwrite = TRUE)
+
+cnae_descricao <- read_csv(here::here("data-raw/cnae_descricao.csv"), 
+                           col_types = cols(Subclasse = "c"))
+usethis::use_data(cnae_descricao, overwrite = TRUE)
+
+grupos_cnaes <- 
+  jsonlite::fromJSON(here::here("data-raw/constants.json"))$grupos_cnae
+usethis::use_data(grupos_cnaes, overwrite = TRUE)
+
