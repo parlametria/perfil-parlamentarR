033b3f841d7cdf34f5e79806e637b56c4dd1d1f5
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2021-01-31 16:50:01 -0300

    Modifica endpoint que retorna dados de votações

--- R/fetcher_proposicoes_votadas_camara.R
+++ R/fetcher_proposicoes_votadas_camara.R
@@ -1,7 +1,7 @@
 #' @title Lista de proposições votadas em um determinado ano
 #' @description Lista as proposições votadas em plenário para um determinado ano
 #' @param ano Ano de ocorrência das votações
-#' @return Dataframe contendo id da proposição, nome e data da votação
+#' @return Dataframe contendo id da proposição e data da última votação
 #' @examples
 #' proposicoes_votadas_em_2019 <- fetch_proposicoes_votadas_camara(2019)
 #' @export
@@ -19,6 +19,7 @@ fetch_proposicoes_votadas_camara <- function(ano = 2019) {
   proposicoes <- tryCatch({
     
    data <- read_delim(url, delim = ";") %>% 
+     filter(siglaOrgao == 'PLEN') %>% 
      rowwise(.) %>% 
      mutate(proposicao_id = stringr::str_split(id, "-")[[1]][1]) %>% 
      ungroup()
@@ -51,60 +52,34 @@ fetch_proposicoes_votadas_camara <- function(ano = 2019) {
   return(proposicoes)
 }
 
-#' @title Recupera xml com votações de uma proposição específica
-#' @description A partir do id da prosição recupera o xml com as votações em plenário da proposição
+#' @title Recupera informações de votações em plenário a partir de um id de proposição
+#' @description A partir do id de uma proposição,
+#' recupera dados de todas as votações disponíveis
 #' @param id_proposicao ID da proposição
-#' @return XML com as votações da proposição
-#' @examples
-#' votacoes_mpv8712019 <- fetch_xml_api_votacao_camara(2190355)
-fetch_xml_api_votacao_camara <- function(id_proposicao) {
-  library(tidyverse)
-  library(RCurl)
-  library(xml2)
-  
-  proposicao <- get_sigla_by_id_camara(id_proposicao) %>%
-    select(siglaTipo, numero, ano)
-  
-  url <- paste0("https://www.camara.leg.br/SitCamaraWS/Proposicoes.asmx/ObterVotacaoProposicao?tipo=",
-                proposicao$siglaTipo, "&numero=", proposicao$numero, "&ano=", proposicao$ano)
-  
-  print(paste0("Baixando votação da ", proposicao$siglaTipo, " ", proposicao$numero, "/", proposicao$ano))
-  
-  xml <- getURL(url) %>%
-    read_xml()
-  
-  return(xml)
-}
-
-#' @title Recupera informações de votações a partir de um xml
-#' @description A partir do xml das votações recupera dados de todas as votações disponíveis
-#' @param id_proposicao ID da proposição
-#' @param xml xml com votações. Se Null então o xml é carregado
 #' @return Info sobre as votações
 #' @examples
-#' votacoes_mpv8712019 <- fetch_votacoes_por_proposicao_camara(2190355, xml)
+#' votacoes_mpv8712019 <- fetch_votacoes_por_proposicao_camara(2190355)
 #' @export
-fetch_votacoes_por_proposicao_camara <- function(id_proposicao, xml = NULL) {
+fetch_votacoes_por_proposicao_camara <- function(id_proposicao) {
   library(tidyverse)
-  library(xml2)
   
-  tryCatch({
-    if (is.null(xml)) {
-      xml <- fetch_xml_api_votacao_camara(id_proposicao)
-    }
+  url_votacoes <- "https://dadosabertos.camara.leg.br/api/v2/proposicoes/%s/votacoes"
+  
+  url <- url_votacoes %>% 
+    sprintf(id_proposicao)
+  
+  votacoes <- tryCatch({
+    data <- (RCurl::getURI(url) %>% fromJSON())$dados
     
-    votacoes <- xml_find_all(xml, ".//Votacao") %>%
-      map_df(function(x) {
-        list(
-          obj_votacao = xml_attr(x, "ObjVotacao"),
-          resumo = xml_attr(x, "Resumo"),
-          cod_sessao = xml_attr(x, "codSessao"),
-          hora = xml_attr(x, "Hora"),
-          data = as.Date(xml_attr(x, "Data"), "%d/%m/%Y")
-        )
-      })
+    data <- data %>% 
+      rowwise(.) %>% 
+      mutate(data = gsub("T", " ", dataHoraRegistro)) %>% 
+      ungroup() %>% 
+      select(id_votacao = id,
+             obj_votacao = descricao,
+             data)
   }, error = function(e) {
-    data <- tribble(~ obj_votacao, ~ resumo, ~ cod_sessao, ~ hora, ~ data)
+    data <- tribble(~ id_votacao, ~ obj_votacao, ~ data)
     return(data)
   })
 
