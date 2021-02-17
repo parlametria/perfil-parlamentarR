9cdfc6e4c52d9416431f8a2d6d05dbc4540c1d51
gileadekelvin <gileadekelvin@gmail.com>
2021-02-17 13:23:36 -0300

    Adiciona função para processar o governismo usando ideal points Adiciona funções para recuperar todos os votos da câmara e do senado em um conjunto de anos

--- data-raw/package_data.R
+++ data-raw/package_data.R
@@ -11,3 +11,8 @@ grupos_cnaes <-
   jsonlite::fromJSON(here::here("data-raw/constants.json"))$grupos_cnae
 usethis::use_data(grupos_cnaes, overwrite = TRUE)
 
+parlamentares <- read_csv(here::here("data-raw/parlamentares.csv"), 
+                           col_types = cols(id_entidade = "c",
+                                            id_entidade_parlametria = "c"))
+usethis::use_data(parlamentares, overwrite = TRUE)
+
