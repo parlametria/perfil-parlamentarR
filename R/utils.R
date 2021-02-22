c87673f20653eef9ae384797122c08279bea631b
gileadekelvin <gileadekelvin@gmail.com>
2021-02-22 08:59:45 -0300

    Exporta função que enumera voto

--- R/utils.R
+++ R/utils.R
@@ -175,6 +175,7 @@ replace_empty_list_for_na <- function(valor) {
 #' @return Dataframe com coluna voto enumerada
 #' @examples
 #' enumera_votacoes(df)
+#' @export
 enumera_voto <- function(df) {
   df %>%
     mutate(
@@ -183,7 +184,7 @@ enumera_voto <- function(df) {
         str_detect(voto, "Sim") ~ 1,
         str_detect(voto, "Obstrução|P-OD") ~ 2,
         str_detect(voto, "Abstenção") ~ 3,
-        str_detect(voto, "Art. 17|art. 51 RISF") ~ 4,
+        str_detect(voto, "Art. 17|art. 51 RISF|Artigo 17") ~ 4,
         str_detect(voto, "Liberado") ~ 5,
         #TODO: Tratar caso P-NRV: Presente mas não registrou foto
         TRUE ~ 0
